0b035f133ba1481dfb4a0981a06fe6cacb5974c9
MAC <horlarmihleykan10@gmail.com>
2023-07-09 01:12:46 +0100

    new

---
+++ user/models.py
@@ -0,0 +1,24 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# import datetime
+#
+#
+# # Create your models here.
+# class User(AbstractUser):
+#
+#     email = models.EmailField(unique=True)
+#     username = models.CharField(max_length=50, unique=True)
+#     password = models.CharField(max_length=20)
+#     registered_on = models.DateTimeField(default=datetime.datetime.now)
+#     confirmed = models.BooleanField(default=False)
+#     confirmed_on = models.DateTimeField(null=True)
+#
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['username']
+#
+#
+#     def __str__(self):
+#         return self.username
+#
+#     def __str__(self):
+#         return self.email
